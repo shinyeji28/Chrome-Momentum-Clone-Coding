4eeec2124f876ed26798bb227d1823bc242d036a
shinyeji28 <xntm10@naver.com>
2022-04-15 12:05:35 +0900

    login with LocalStorage

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const h1 = document.querySelector("div.hello:first-child h1");
-function handleTitleClick(){
-    // const clickedClass = "clicked";
-    // if(h1.classList.contains(clickedClass)){
-    //     h1.classList.remove(clickedClass);
-    // } else {
-    //     h1.classList.add(clickedClass);
-    // }
-    h1.classList.toggle("clicked");  // 클래스 토글
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintCreetings(username);
 }
 
-h1.addEventListener("click", handleTitleClick);
+function paintCreetings(username){
+    greeting.innerText = `Hello ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintCreetings(savedUsername);
+}
